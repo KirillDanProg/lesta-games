658ad7a6716f44ea39919e78ef614e249af85a85
KirillDanProg <kirill2898v@gmail.com>
2025-01-26 19:41:10 +0300

    feat: add clear search filter icon

--- src/features/search.filter/ui/search.filter.tsx
+++ src/features/search.filter/ui/search.filter.tsx
@@ -1,4 +1,8 @@
+import CloseIcon from "@/shared/assets/icons/close-icon.svg?react";
+import { useDebounce } from "@/shared/lib/hooks/use.debounce";
 import { Input } from "@/shared/ui";
+import { useEffect, useState } from "react";
+import s from "./search.filter.module.scss";
 
 type SearchFilterProps = {
   onFilterByTitle: (title: string) => void;
@@ -7,5 +11,25 @@ type SearchFilterProps = {
 export const SearchFilter = (props: SearchFilterProps) => {
   const { onFilterByTitle } = props;
 
-  return <Input changeCallback={onFilterByTitle} />;
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value);
+
+  const changeCallback = (value: string) => {
+    setValue(value);
+  };
+
+  useEffect(() => {
+    onFilterByTitle(debouncedValue.toLowerCase().trim());
+  }, [debouncedValue]);
+
+  const onClearSearchFilter = () => {
+    setValue("");
+  };
+
+  return (
+    <div className={s.searchFilterContainer}>
+      <CloseIcon className={s.closeIcon} onClick={onClearSearchFilter} />
+      <Input value={value} changeCallback={changeCallback} className={s.searchFilterInput} />
+    </div>
+  );
 };
